13c50752b4a036c496d2f4b99e9d38c7ce281d8d
Vishwas Charan <vishwascharan11@gmail.com>
2025-08-20 00:08:14 +0530

    deleted some extra files

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./views/**/*.ejs'], // Adjust paths based on your project
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
